f229c8bd411b5edb846c52e7e8a49356cfbc3679
bh1ff <ahmedayad1111@gmail.com>
2023-09-19 17:01:33 +0100

    function getweather

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const apiKey = "0b26a0d735f1c68e879212c2650e5b40";
 let cities = [];
+
+function getWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => {
+            // Use the data to display weather
+            displayWeather(data, cityName);
+        });
+}
