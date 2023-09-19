832abdab49470f224b76de9b8a83324c8b575b11
bh1ff <ahmedayad1111@gmail.com>
2023-09-19 17:35:42 +0100

    failed css

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ let cities = [];
 
 // Function to fetch weather data for a given city
 function getWeather(cityName) {
-    // Construct the API URL
+    // Construct the API URL for current weather
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
     
-    // Fetch data from the API
+    // Fetch data from the API for current weather
     fetch(queryURL)
         .then(response => response.json())
         .then(data => {
-            // Display the weather data
+            // Display the current weather data
             displayWeather(data, cityName);
+            
+            // Fetch the 5-day forecast using the city's coordinates
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}&units=imperial`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Display the 5-day forecast
+            displayForecast(data);
         });
 }
 
-// Function to display the weather data
+// Function to display the current weather data
 function displayWeather(data, cityName) {
     let todayEl = document.getElementById("today");
     todayEl.innerHTML = "";
@@ -47,6 +55,42 @@ function displayWeather(data, cityName) {
 
     // Save the city to local storage
     saveCity(cityName);
+}
+
+// New function to display the 5-day forecast
+function displayForecast(data) {
+    let forecastEl = document.getElementById("forecast");
+    forecastEl.innerHTML = "";
+
+    for (let i = 0; i < data.list.length; i += 8) {
+        let dayData = data.list[i];
+        let date = dayjs(dayData.dt_txt).format('M/D/YYYY');
+        let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+        let temp = dayData.main.temp;
+        let humidity = dayData.main.humidity;
+
+        let dayEl = document.createElement("div");
+        dayEl.classList.add("col-md-2.4", "m-1", "bg-primary", "text-white", "rounded", "p-2");
+
+        let dateEl = document.createElement("h4");
+        dateEl.textContent = date;
+        dayEl.appendChild(dateEl);
+
+        let iconEl = document.createElement("img");
+        iconEl.setAttribute("src", iconURL);
+        iconEl.setAttribute("alt", dayData.weather[0].description);
+        dayEl.appendChild(iconEl);
+
+        let tempEl = document.createElement("p");
+        tempEl.textContent = `Temp: ${temp}°F`;
+        dayEl.appendChild(tempEl);
+
+        let humidityEl = document.createElement("p");
+        humidityEl.textContent = `Humidity: ${humidity}%`;
+        dayEl.appendChild(humidityEl);
+
+        forecastEl.appendChild(dayEl);
+    }
 }
 
 // Function to save city to local storage
