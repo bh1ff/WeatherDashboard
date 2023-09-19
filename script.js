30ebc52edd6a579b65a2b9ce35c11eda19f3f9fa
bh1ff <ahmedayad1111@gmail.com>
2023-09-19 17:15:44 +0100

    added appendages

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ let cities = [];
 
 function getWeather(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-    console.log(queryURL)
     fetch(queryURL)
         .then(response => response.json())
         .then(data => {
-            // Use the data to display weather
             displayWeather(data, cityName);
         });
 }
 
 function displayWeather(data, cityName) {
+    let todayEl = document.getElementById("today");
+    todayEl.innerHTML = "";
 
-    document.getElementById("today").innerHTML = "";
+    // Extracting data from the API response
+    let date = dayjs().format('M/D/YYYY');
+    let iconURL = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    let temp = data.main.temp;
+    let humidity = data.main.humidity;
+    let windSpeed = data.wind.speed;
 
+    // Creating and appending elements to display the extracted data
+    let titleEl = document.createElement("h3");
+    titleEl.innerHTML = `${cityName} (${date})<img src="${iconURL}" alt="${data.weather[0].description}">`;
+    todayEl.appendChild(titleEl);
+
+    let tempEl = document.createElement("p");
+    tempEl.textContent = `Temperature: ${temp}°F`;
+    todayEl.appendChild(tempEl);
+
+    let humidityEl = document.createElement("p");
+    humidityEl.textContent = `Humidity: ${humidity}%`;
+    todayEl.appendChild(humidityEl);
+
+    let windSpeedEl = document.createElement("p");
+    windSpeedEl.textContent = `Wind Speed: ${windSpeed} MPH`;
+    todayEl.appendChild(windSpeedEl);
 
     saveCity(cityName);
 }
@@ -26,7 +47,7 @@ function saveCity(cityName) {
         localStorage.setItem("cities", JSON.stringify(cities));
     }
 }
-// adding event listeners
+
 document.getElementById("search-button").addEventListener("click", function(event) {
     event.preventDefault();
     let city = document.getElementById("search-input").value.trim();
@@ -43,7 +64,6 @@ document.getElementById("history").addEventListener("click", function(event) {
     }
 });
 
-// initialisation
 function init() {
     let storedCities = JSON.parse(localStorage.getItem("cities"));
     if (storedCities !== null) {
